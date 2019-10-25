953461ee23dd3f6cf6224575af9292c580374349
pynkvalerian <pynkvalerian@gmail.com>
2019-10-25 10:32:45 +0800

    track dist folder

---
+++ dist/index.js
@@ -0,0 +1,75 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Line = exports.Scatter = exports.Pie = exports.Donut = exports.BarH = exports.Bar = void 0;
+
+var _react = _interopRequireWildcard(require("react"));
+
+var rv = _interopRequireWildcard(require("rough-viz/dist/roughviz.min"));
+
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; if (obj != null) { var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
+function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
+var generateId = function generateId(prefix) {
+  return "".concat(prefix).concat(('' + Math.random()).split('.')[1]);
+};
+
+var wrap = function wrap(rvComp) {
+  return function (_ref) {
+    var prefix = _ref.prefix,
+        props = _objectWithoutProperties(_ref, ["prefix"]);
+
+    var ref = (0, _react.useRef)();
+
+    var _useState = (0, _react.useState)(generateId(prefix || 'roughviz-')),
+        _useState2 = _slicedToArray(_useState, 1),
+        id = _useState2[0];
+
+    (0, _react.useEffect)(function () {
+      if (ref.current) {
+        new rvComp(_objectSpread({
+          element: '#' + id
+        }, props));
+      }
+    }, [ref, id, props]);
+    return _react.default.createElement("div", {
+      id: id,
+      ref: ref
+    });
+  };
+};
+
+var Bar = wrap(rv.Bar);
+exports.Bar = Bar;
+var BarH = wrap(rv.BarH);
+exports.BarH = BarH;
+var Donut = wrap(rv.Donut);
+exports.Donut = Donut;
+var Pie = wrap(rv.Pie);
+exports.Pie = Pie;
+var Scatter = wrap(rv.Scatter);
+exports.Scatter = Scatter;
+var Line = wrap(rv.Line);
+exports.Line = Line;
+//# sourceMappingURL=index.js.map
